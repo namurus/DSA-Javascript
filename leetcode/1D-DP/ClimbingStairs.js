908e9c7e57162d1cd30b35e484112755f7353eba
namurus <thaidinhngan@gmail.com>
2025-10-26 23:29:58 +0700

    Add alternative solution for ClimbingStairs

--- leetcode/1D-DP/ClimbingStairs.js
+++ leetcode/1D-DP/ClimbingStairs.js
@@ -5,6 +5,8 @@
  * @param {number} n
  * @return {number}
  */
+
+// This solution is good when you want to reuse the computed values later
 var climbStairs = function(n) {
     let memArray = new Array(n+1);
     memArray[0] = 1;
@@ -14,4 +16,21 @@ var climbStairs = function(n) {
         memArray[i] = memArray[i-1] + memArray[i-2];
     }
     return memArray[n];
+};
+
+// This solution does not use extra space
+var climbStairs = function(n) {
+    if (n <= 3) return n;
+
+    let prev1 = 3;
+    let prev2 = 2;
+    let cur = 0;
+
+    for (let i = 3; i < n; i++) {
+        cur = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = cur;
+    }
+
+    return cur;    
 };
